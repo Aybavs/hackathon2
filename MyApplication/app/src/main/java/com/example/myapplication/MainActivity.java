9d5eff63fbbcdff31d98565729cedafdd0ea21ca
baturay incekara <99458931+thirtyfive-35@users.noreply.github.com>
2024-04-28 21:33:12 +0300

    main-login-gecis

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ((EditText)findViewById(R.id.email)).getText().toString();
+                String password = ((EditText)findViewById(R.id.parola)).getText().toString();
+                loginUser(email, password);
+            }
+        });
+    }
+
+    public void loginUser(String email, String password) {
+        String url = "http://93.95.26.206:8080/login";
+
+        try {
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("user_email_address", email);
+            jsonBody.put("user_password", password);
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.POST, url, jsonBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                String token = response.getString("token");
+                                Log.d("Token", token);
+                                // Token'i kullanarak bir sonraki ekrana geçiş yap
+                                Intent intent = new Intent(MainActivity.this, MainPage.class);
+                                intent.putExtra("TOKEN", token);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("Giriş Hatası", error.toString());
+                        }
+                    }
+            );
+
+            requestQueue.add(jsonObjectRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
